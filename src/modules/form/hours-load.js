bfeb23bf07f832f3c1ae184d9d53504fb08a89a2
Rafaa Ehlke <heeyrafaa@gmail.com>
2024-10-22 20:41:21 -0300

    criado seleçao de horários

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,5 +1,8 @@
 import { openingHours } from "../../utils/opening-hours.js"
 import dayjs from "dayjs"
+import { hoursClick } from "./hours-click.js" 
+
+const hours = document.getElementById("hours")
 
 export function hoursLoad({ date }) {
   const opening = openingHours.map((hour) => {
@@ -13,4 +16,36 @@ export function hoursLoad({ date }) {
       available: !isHourPast
     })
   })
+
+  // Renderiza os Horários.
+
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement("li")
+
+    li.classList.add("hour")
+    li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+    li.textContent = hour
+
+    if (hour === "07:00") {
+      hourHeaderAdd("Manhã")
+    }else if (hour === "13:30") {
+      hourHeaderAdd("Tarde")
+    } else if (hour === "18:00") {
+      hourHeaderAdd("Noite")
+    }
+
+    hours.append(li)
+  })
+
+  hoursClick()
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement("li")
+
+  header.classList.add("hour-period")
+  header.textContent = title
+
+  hours.append(header)
 }
